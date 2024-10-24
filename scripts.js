2482ad352681d2f2353f8a05c98eaefa231be379
Saimon Ahmed <67533318+SAIMON-AHMED@users.noreply.github.com>
2024-10-24 10:07:36 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 // Time -> O(n^2) | Space -> O(1)
 function bubbleSort(array) {
-
+  
   let swap = true;
+  let counter = 0;
   while (swap) {
     swap = false;
-    for (let i = 0; i < array.length - 1; i++) {
+    for (let i = 0; i < array.length - 1 - counter; i++) {
       if (array[i] > array[i + 1]) {
         let temp = array[i];
         array[i] = array[i + 1];
@@ -12,6 +13,8 @@ function bubbleSort(array) {
         swap = true;
       } 
     }
+    counter++;
   }
   return array;
 }
+
